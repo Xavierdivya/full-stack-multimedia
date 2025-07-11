0bc548f11c1d359eafc62d0d480685020bf6f016
Divya <146223550+Xavierdivya@users.noreply.github.com>
2025-07-11 14:27:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+server.listen(5001, () => {
+    console.log('Server is running on http://localhost:5001');
 });
